c4590d00447ee510a2f94956c8fc54aedd919511
Frankium <tis7bfrankie@gmail.com>
2022-10-23 20:56:38 +1100

    add task 1 and 2 solutions

---
+++ city_country_csv_reader.py
@@ -0,0 +1,137 @@
+from locations import City, Country, test_example_countries_and_cities
+
+def create_cities_countries_from_CSV(path_to_csv: str) -> None:
+    """
+    Reads a CSV file given its path and creates instances of City and Country for each line.
+    """
+    # read raw file content
+    with open(path_to_csv) as f:
+        raw = f.read()
+
+    # parse data
+    data = parse_raw_csv(raw)
+
+    # register data to countries and cities
+    for record in data["records"]:
+        # register country if not exists in registry
+        if not (record["country"] in Country.countries):
+            Country(record["country"], record["iso3"])
+        
+        # register city
+        City(record["city_ascii"], record["lat"], record["lng"], record["country"], record["capital"], record["id"])
+
+def parse_row(raw_row: str, deliminater: str = ",") -> list[str]:
+    """
+    Takes in a string containing a raw csv row, parse into a list of strings
+    Use the deliminater argument to override default deliminater of comma
+    if a cell is surrounded with quotes, deliminater is treated as literals
+    if double quote appered in a cell, double quote is treated as quote literal escape
+    return the result in list of strings
+    """
+    cells = []
+    # parse line by line
+    in_quote = False
+    cell_item = ""
+    i = 0
+    while i < len(raw_row):
+        char = raw_row[i]
+
+        # check if cell starts with 3 quotes
+        if not cell_item and i + 2 < len(raw_row):
+            if raw_row[i:i+3] == '"""':
+                cell_item += '"'
+                in_quote = True
+                i += 3
+                continue
+
+
+        # check if quote is quote escape
+        if (i + 1) != len(raw_row):
+            next_char = raw_row[i + 1]
+            if char == '"' and next_char == '"':
+                cell_item += '"'
+                i += 2
+                continue
+
+            # check if cell quote ends by checking leading deliminater
+            if in_quote and char == '"' and next_char == deliminater:
+                in_quote = False
+                cells.append(cell_item)
+                cell_item = ""
+                i += 2
+                continue
+
+
+        # check if cell starts with quotes
+        if char == '"' and not cell_item:
+            in_quote = True
+            i += 1
+            continue
+        
+        # check if cell quote ends by checking end of line
+        if in_quote and char == '"' and (i + 1) == len(raw_row):
+            in_quote = False
+            cells.append(cell_item)
+            cell_item = ""
+            i += 1
+            continue
+
+        # check for deliminater outside quote
+        if not in_quote and char == deliminater:
+            cells.append(cell_item)
+            cell_item = ""
+            i += 1
+            continue
+        
+        cell_item += char
+
+        i += 1
+    
+    # check if last cell has item
+    if cell_item:
+        cells.append(cell_item)
+
+    return cells
+
+def parse_raw_csv(raw_csv: str) -> dict:
+    """
+    Takes in a string containing the raw csv, seraialize into a dict with records (like a json)
+    Applicable to all purpose csv where the first line contains column name / field name
+    return the result in dict
+    The content of the result dict has 2 fields:
+     * "column_names": a list of string containing column names ordered from left to right
+     * "records": a list of dict containing row records, where each dict has a key of column name and value of string value in that column
+    """
+
+    rows = raw_csv.splitlines()
+    # check if nothing to parse
+    if not rows:
+        return {}
+    
+    column_names = parse_row(rows[0])
+    data_rows = rows[1:]
+    data_cells = [parse_row(data_row) for data_row in data_rows]
+
+    result = {
+        "column_names": column_names,
+        "records": []
+    }
+
+    for row in data_cells:
+        record = {}
+        for i in range(len(row)):
+            cell = row[i]
+            column = column_names[i]
+            record[column] = cell
+        result["records"].append(record)
+        record = {}
+
+    return result
+
+
+if __name__ == "__main__":
+    create_cities_countries_from_CSV("worldcities_truncated.csv")
+    for i in Country.countries:
+        print(i)
+        for j in Country.countries[i].cities:
+            print(f"->\t{j}")
---
+++ locations.py
@@ -0,0 +1,155 @@
+from __future__ import annotations 
+from enum import Enum
+from geopy.distance import great_circle
+
+
+class CapitalType(Enum):
+    """
+    The different types of capitals (e.g. "primary").
+    """
+    primary = "primary"
+    admin = "admin"
+    minor = "minor"
+    unspecified = ""
+
+    def __str__(self) -> str:
+        return self.value
+
+
+class Country():
+    """
+    Represents a country.
+    """
+
+    countries: dict[str, Country] = dict() # a dict that associates country names to instances.
+    name: str
+    iso3: str
+    cities: list[City]
+
+    def __init__(self, name: str, iso3: str) -> None:
+        """
+        Creates an instance with a country name and a country ISO code with 3 characters.
+        """
+        self.name = name
+        self.iso3 = iso3.upper()
+        self.cities = []
+        Country.countries[name] = self
+
+    def _add_city(self, city: City):
+        """
+        Adds a city to the country.
+        """
+        self.cities.append(city)
+
+    def get_cities(self, capital_types: list[CapitalType] = None) -> list[City]:
+        """
+        Returns a list of cities of this country.
+
+        The argument capital_types can be given to specify a subset of the capital types that must be returned.
+        Cities that do not correspond to these capital types are not returned.
+        If no argument is given, all cities are returned.
+        """
+        # if capital_types not specified, return a all cities
+        if not capital_types:
+            return self.cities
+
+        # if capital_types specified, filter and return a list of cities
+        return list(filter(lambda city: city.capital_type in capital_types, self.cities))
+
+    def get_city(self, city_name: str) -> City:
+        """
+        Returns a city of the given name in this country.
+        Returns None if there is no city by this name.
+        If there are multiple cities of the same name, returns an arbitrary one.
+        """
+        cities = list(filter(lambda city: city.name == city_name, self.cities))
+        if not cities:
+            return None
+        
+        return cities[0]
+
+    def __str__(self) -> str:
+        """
+        Returns the name of the country.
+        """
+        return self.name
+
+
+class City():
+    """
+    Represents a city.
+    """
+
+    cities: dict[str, City] = dict() # a dict that associates city IDs to instances.
+    coordinate: tuple[float, float] # a vector for the coordinate. [0] is latitude, and [1] is longditude
+    capital_type: CapitalType
+    country: Country
+    city_id: str
+
+    def __init__(self, name: str, latitude: str, longitude: str, country: str, capital_type: str, city_id: str) -> None:
+        """
+        Initialises a city with the given data.
+        """
+
+        self.name = name
+        self.coordinate = (float(latitude), float(longitude))
+        self.country = Country.countries[country]
+        self.city_id = city_id
+        City.cities[city_id] = self
+
+        # check if capital type is defined in enum
+        if capital_type in CapitalType.__members__:
+            self.capital_type = CapitalType[capital_type]
+        else:
+            self.capital_type = CapitalType.unspecified
+
+        # add city to country
+        self.country._add_city(self)
+
+    def distance(self, other_city: City) -> int:
+        """
+        Returns the distance in kilometers between two cities using the great circle method,
+        rounded up to an integer.
+        """
+        d = great_circle(self.coordinate, other_city.coordinate).kilometers
+        return round(d)
+
+    def __str__(self) -> str:
+        """
+        Returns the name of the city and the country ISO3 code in parentheses.
+        For example, "Melbourne (AUS)".
+        """
+        return f"{self.name} ({self.country.iso3})"
+
+
+def create_example_countries_and_cities() -> None:
+    """
+    Creates a few Countries and Cities for testing purposes.
+    """
+    australia = Country("Australia", "AUS")
+    melbourne = City("Melbourne", "-37.8136", "144.9631", "Australia", "admin", "1036533631")
+    canberra = City("Canberra", "-35.2931", "149.1269", "Australia", "primary", "1036142029")
+    sydney = City("Sydney", "-33.865", "151.2094", "Australia", "admin", "1036074917")
+
+    japan = Country ("Japan", "JPN")
+    tokyo = City("Tokyo", "35.6839", "139.7744", "Japan", "primary", "1392685764")
+
+
+def test_example_countries_and_cities() -> None:
+    """
+    Assuming the correct cities and countries have been created, runs a small test.
+    """
+    australia = Country.countries['Australia']
+    canberra =  australia.get_city("Canberra")
+    melbourne = australia.get_city("Melbourne")
+    sydney = australia.get_city("Sydney")
+
+    print("The distance between {} and {} is {}km".format(melbourne, sydney, melbourne.distance(sydney)))
+
+    for city in australia.get_cities([CapitalType.admin, CapitalType.primary]):
+        print("{} is a {} capital of {}".format(city, city.capital_type, city.country))
+
+
+if __name__ == "__main__":
+    create_example_countries_and_cities()
+    test_example_countries_and_cities()
